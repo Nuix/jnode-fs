f6e0b9bb81a84d18d1a360a79d5ca09f2baf895c
Magnus Jahnen <jahnen@in.tum.de>
2017-02-28 19:00:02 -0800

    Add dummy Device

---
+++ src/main/java/org/jnode/driver/ApiNotFoundException.java
@@ -0,0 +1,58 @@
+/*
+ * $Id$
+ *
+ * Copyright (C) 2003-2015 JNode.org
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but 
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public 
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; If not, write to the Free Software Foundation, Inc., 
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+ 
+package org.jnode.driver;
+
+/**
+ * API has not been found exception.
+ *
+ * @author Ewout Prangsma (epr@users.sourceforge.net)
+ */
+public class ApiNotFoundException extends DeviceException {
+
+    /**
+     *
+     */
+    public ApiNotFoundException() {
+        super();
+    }
+
+    /**
+     * @param message
+     * @param cause
+     */
+    public ApiNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    /**
+     * @param cause
+     */
+    public ApiNotFoundException(Throwable cause) {
+        super(cause);
+    }
+
+    /**
+     * @param s
+     */
+    public ApiNotFoundException(String s) {
+        super(s);
+    }
+}
