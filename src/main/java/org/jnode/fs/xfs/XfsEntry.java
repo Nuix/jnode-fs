5932424a4e468d9fc30bbdc56c43066f6ddb3f68
ricardo.garza <ricardogarzaverastegui@gmail.com>
2022-01-27 16:33:53 -0600

    fix to read sparse files

--- src/main/java/org/jnode/fs/xfs/XfsEntry.java
+++ src/main/java/org/jnode/fs/xfs/XfsEntry.java
@@ -173,8 +173,7 @@ public class XfsEntry extends AbstractFSEntry implements FSEntryCreated, FSEntry
      */
     private void readFromExtentList(long offset, ByteBuffer destBuf) throws IOException {
         long blockSize = fileSystem.getSuperblock().getBlockSize();
-        long extentOffset = 0;
-        int bytesToRead = 0;
+        int bytesToRead;
 
         for (DataExtent extent : extentList) {
             if (!destBuf.hasRemaining()) {
@@ -184,6 +183,7 @@ public class XfsEntry extends AbstractFSEntry implements FSEntryCreated, FSEntry
             if (extent.isWithinExtent(offset, blockSize)) {
                 ByteBuffer readBuffer = destBuf.duplicate();
 
+                long extentOffset = extent.getStartOffset() * blockSize;
                 long offsetWithinBlock = offset - extentOffset;
                 if ((extent.getBlockCount() * blockSize - offsetWithinBlock) > 0) {
                     bytesToRead = (int) Math.min(extent.getBlockCount() * blockSize - offsetWithinBlock, destBuf.remaining());
@@ -197,9 +197,6 @@ public class XfsEntry extends AbstractFSEntry implements FSEntryCreated, FSEntry
                 offset += bytesToRead;
                 destBuf.position(destBuf.position() + bytesToRead);
             }
-
-            long extentLength = extent.getBlockCount() * blockSize;
-            extentOffset += extentLength;
         }
     }
 
